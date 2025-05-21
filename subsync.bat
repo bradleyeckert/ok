92a6fec4a02a32dc38fcd8e4219364aa97d961f3
Brad Eckert <brad.sunvn@gmail.com>
2025-05-20 19:18:52 -0700

    Changed submodule head

--- subsync.bat
+++ subsync.bat
@@ -1,7 +1,7 @@
 rem clean up any .o files left by make failure
-del /s /q *.o
+rem (or not) del /s /q *.o
 rem change submodules to the latest and greatest, discarding local changes
-git submodule update --force --recursive
+git submodule update --init --recursive
 git submodule update --remote --recursive
 git add --all
 git commit -m "Changed submodule head"
