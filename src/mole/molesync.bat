d8703acabbb49baaa2b0a7a2bef13e09e66fb9ec
Brad Eckert <brad.sunvn@gmail.com>
2025-05-23 15:32:54 -0700

    Update molesync.bat

--- src/mole/molesync.bat
+++ src/mole/molesync.bat
@@ -1,4 +1,13 @@
 @echo off
+echo Only run this batch file if you want to overwrite files
+echo with the latest from the local mole repo.
+echo.
+set /P yesno=Do you want to continue? (y/n) 
+if /i "%yesno%"=="y" goto continue
+echo Exiting...
+exit
+:continue
+
 if exist ..\..\..\mole (
   copy ..\..\..\mole\src\mole.c mole.c /y
   copy ..\..\..\mole\src\mole.h mole.h /y
--- subsync.bat
+++
@@ -1,9 +0,0 @@
-rem clean up any .o files left by make failure
-rem (or not) del /s /q *.o
-rem change submodules to the latest and greatest, discarding local changes
-git submodule update --init --recursive
-git submodule update --force --recursive
-git submodule update --remote --recursive
-git add --all
-git commit -m "Changed submodule head"
-git push
