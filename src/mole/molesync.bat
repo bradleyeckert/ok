7851f5c584a896693ae61a6b7fe0316ba96c63f4
Brad Eckert <brad.sunvn@gmail.com>
2025-05-23 14:26:43 -0700

    Update molesync.bat

--- src/mole/molesync.bat
+++ src/mole/molesync.bat
@@ -3,10 +3,10 @@ if exist ..\..\..\mole (
   copy ..\..\..\mole\src\mole.c mole.c /y
   copy ..\..\..\mole\src\mole.h mole.h /y
   copy ..\..\..\mole\src\moleconfig.h moleconfig.h /y
-  copy ..\..\..\mole\src\blake2s\src\blake2s.c blake2s.c /y
-  copy ..\..\..\mole\src\blake2s\src\blake2s.h blake2s.h /y
-  copy ..\..\..\mole\src\xchacha\src\xchacha.c xchacha.c /y
-  copy ..\..\..\mole\src\xchacha\src\xchacha.h xchacha.h /y
+  copy ..\..\..\mole\src\blake2s.c blake2s.c /y
+  copy ..\..\..\mole\src\blake2s.h blake2s.h /y
+  copy ..\..\..\mole\src\xchacha.c xchacha.c /y
+  copy ..\..\..\mole\src\xchacha.h xchacha.h /y
 ) else (
   echo Missing mole folder, please clone the repo
 )
